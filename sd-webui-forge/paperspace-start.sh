a660aa6d02ca07749b9d8f36629a7ef184ceba00
ffxvs <156585597+ffxvs@users.noreply.github.com>
2024-09-10 05:28:35 +0700

    Update paperspace-start.sh

--- sd-webui-forge/paperspace-start.sh
+++ sd-webui-forge/paperspace-start.sh
@@ -17,6 +17,7 @@ download_notebooks() {
 
 # Start jupyter lab
 start_jupyter() {
+    cd / || exit
     echo "Starting Jupyter Lab..."
     jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False \
         --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True --FileContentsManager.delete_to_trash=False \
