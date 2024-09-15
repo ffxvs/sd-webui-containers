7a71825c70877487ec6ef507f1e3f994096407da
ffxvs <156585597+ffxvs@users.noreply.github.com>
2024-09-15 21:20:43 +0700

    Rework SD Web UI Auto1111 container

--- sd-webui/paperspace-start.sh
+++ sd-webui/paperspace-start.sh
@@ -4,19 +4,21 @@
 #                          Function Definitions                                #
 # ---------------------------------------------------------------------------- #
 
+branch="dev"
 main_dir="/notebooks"
 
 # Download notebooks
 download_notebooks() {
     cd /internal || exit
-    wget -nv -O main.py https://raw.githubusercontent.com/ffxvs/sd-webui-complete-setup/dev/internal/main.py
-    wget -nv -O on-completed.sh https://raw.githubusercontent.com/ffxvs/sd-webui-complete-setup/dev/internal/on-completed.sh
-    wget -nv -O notebooks_updater.py https://raw.githubusercontent.com/ffxvs/sd-webui-containers/dev/sd-webui-forge/notebooks_updater.py
+    wget -nv -O main.py https://raw.githubusercontent.com/ffxvs/sd-webui-complete-setup/$branch/internal/main.py
+    wget -nv -O on-completed.sh https://raw.githubusercontent.com/ffxvs/sd-webui-complete-setup/$branch/internal/on-completed.sh
+    wget -nv -O notebooks_updater.py https://raw.githubusercontent.com/ffxvs/sd-webui-containers/$branch/sd-webui/notebooks_updater.py
     python notebooks_updater.py --paperspace
 }
 
 # Start jupyter lab
 start_jupyter() {
+  cd / || exit
     echo "Starting Jupyter Lab..."
     jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False \
         --ServerApp.allow_remote_access=True --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True --FileContentsManager.delete_to_trash=False \
